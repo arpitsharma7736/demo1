3ae24991a0736d6b4bbd848d613fff57c0aab63c
Arpit Sharma <arpitsharma7736@gmail.com>
2023-09-20 11:54:49 +0530

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <section className="hero">
+        <h2 className='con-head'>Welcome</h2>
+        <p>Connecting Jat individuals and families for a brighter future.</p>
+    </section>
+    </>
+  )
+}
+
+export default Home
